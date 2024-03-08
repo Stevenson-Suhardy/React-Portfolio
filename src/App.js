3bfa079ee2a67358625c8a7a58ea9835f6cfa625
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2024-03-07 19:57:46 -0500

    Added responsive navbar, and social media icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hello</h1>
+      <Navbar />
+      <Home />
     </div>
   );
 }
